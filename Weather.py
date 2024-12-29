462d901b21d5f660a1adc390f4e14337d4ed6820
Noaga1980 <gannotnoa@gmail.com>
2024-12-29 19:28:56 +0200

    save my project

---
+++ Weather.py
@@ -0,0 +1,35 @@
+
+
+import requests
+
+# Ask API from the User
+api_key = input("What is your API key? ")
+
+# Ask city Name From the User
+city = input("Enter the city name: ")
+
+# The URL
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+# Sent the request to API
+response = requests.get(url)
+
+# Check if the request success
+if response.status_code == 200:
+    # Converting the response to JSON format
+    data = response.json()
+
+    # Import the relevant data
+    city_name = data['name']
+    temperature = data['main']['temp']
+    weather_description = data['weather'][0]['description']
+    humidity = data['main']['humidity']
+
+    # print the results
+    print(f"Weather in {city_name}:")
+    print(f"Temperature: {temperature}°C")
+    print(f"Description: {weather_description}")
+    print(f"Humidity: {humidity}%")
+else:
+    # Error handling
+    print(f"Failed to retrieve data: {response.status_code}")
